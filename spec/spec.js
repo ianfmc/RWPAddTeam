285a906ab3f850a95a780600c4b6f1a39f92cb0d
Ian McCallum <ianfmc@gmail.com>
2016-10-21 13:43:28 -0700

    Add Test (Mocha+expect.js)

---
+++ spec/spec.js
@@ -0,0 +1,24 @@
+var app = require('../index.js');
+var expect = require('expect.js');
+ 
+describe('Add a New Team', function() { 
+
+	before(function(){
+	    });
+
+	after(function() {
+	    });
+
+	beforeEach(function() {
+	    });
+
+	afterEach(function() {
+	    });
+
+       it('--Adds a team with correct data', function(done) {
+	       expect().fail("No Test Created");
+	       done();
+	    });
+    });
+
+
